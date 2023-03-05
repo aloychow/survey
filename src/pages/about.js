df54f05c5861f29a85b7addfca2df65075df71bb
aloychow <aloychow@outlook.com>
2023-03-05 20:42:58 +0800

    test

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
-import Navbar from 'components/Navbar'
 import styled from 'styled-components'
 import Link from "next/link";
 
--- src/pages/survey-1.js
+++ src/pages/survey-1.js
@@ -26,7 +26,7 @@ const MainContainer = styled.div`
 `
 
 const ContentText = styled.div`
-color: #000;
+  color: #000;
   font-size: 1.5rem;
   font-weight: 500;
   text-align: center;
